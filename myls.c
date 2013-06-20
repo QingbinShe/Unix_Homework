d8f476b9d8913d8aa1e00d9be8e9f06b35350593
sheqingbin <sheqingbin@ubuntu.ubuntu-domain>
2013-06-20 21:14:38 +0800

    realize part of ls -l filename

--- myls.c
+++ myls.c
@@ -1,17 +1,21 @@
 #include <dirent.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/stat.h>
+#include <unistd.h>
 //#include "figB.3"
 
 int main(int argc, char *argv[])
 {
     DIR *dp;
     struct dirent *dirp;
+    struct stat buf; //buffer the stat of direction
 
 //the commend is "myls"
     if (argc == 1){
         if ((dp = opendir(".")) == NULL){
             printf("can't open current direction\n");
+            exit(0);
         }
         while ((dirp = readdir(dp)) != NULL)
             printf("%s	", dirp -> d_name);
@@ -23,7 +27,8 @@ int main(int argc, char *argv[])
     if (argc == 2){
         if ((dp = opendir(argv[1])) == NULL){
             //err_sys("can't open %s", argv[1]);
-            printf("cna't open %s", argv[1]);
+            printf("can't open %s", argv[1]);
+            exit(0);
         }
         while ((dirp = readdir(dp)) != NULL)
             printf("%s	", dirp -> d_name);
@@ -36,10 +41,29 @@ int main(int argc, char *argv[])
     if (argc == 3){
         switch (*(argv[1] + 1))
         {
-        case 'l' ://{printf ("%s\n", argv[1]); exit(0);}
-        {
-            
+//ls -l filename
+        case 'l' :{
+        //printf ("%s\n", argv[1]); exit(0);
+        if ((dp = opendir(argv[2])) == NULL){
+        printf("can't open %s", argv[1]);
+        exit(0);
         }
+        while ((dirp = readdir(dp)) != NULL){
+            stat("dirp -> d_name", &buf);
+            printf("%d	%d	%d	", buf.st_mode, buf.st_uid, buf.st_gid);
+            printf("%d	%d	%s	", buf.st_size, buf.st_mtime, dirp ->d_name);
+            printf("\n");
+        }
+        printf("\n");
+        exit(0);
+        }
+
+//ls -t filename
+        case 't' :{
+                
+        }
+
+//commend is wrong
         defaule : exit(0);
         }
     }
