178943796452420056a2f8c1dca35859931a768a
sheqingbin <sheqingbin@ubuntu.ubuntu-domain>
2013-06-25 10:43:56 +0800

    final

--- myls.c
+++ myls.c
@@ -5,6 +5,11 @@
 #include <unistd.h>
 //#include "figB.3"
 
+#include<time.h> 
+#define D (24*60*60)
+#define H (60*60)
+#define M (60)
+
 int main(int argc, char *argv[])
 {
     DIR *dp;
@@ -48,11 +53,22 @@ int main(int argc, char *argv[])
         printf("can't open %s", argv[1]);
         exit(0);
         }
+        printf("authority	UID	GID	size	modifingtime	filename\n");
         while ((dirp = readdir(dp)) != NULL){
             stat("dirp -> d_name", &buf);
+ 
+            time(&(buf.st_mtime)); 
+            long time = buf.st_mtime % D;
+            int h = time / H;
+            time = time % H;
+            int m = time / M;
+            time = time % M;
+            int s = time;
+
             printf("%d	%d	%d	", buf.st_mode, buf.st_uid, buf.st_gid);
-            printf("%d	%d	%s	", buf.st_size, buf.st_mtime, dirp ->d_name);
+            printf("%ld	%02d:%02d:%02d	%s	", buf.st_size, h, m, s, dirp ->d_name);
             printf("\n");
+            //printf("%d", sizeof(off_t));
         }
         printf("\n");
         exit(0);
@@ -60,7 +76,45 @@ int main(int argc, char *argv[])
 
 //ls -t filename
         case 't' :{
-                
+        
+        }
+
+//ls -r filename
+        case 'r' :{
+        if ((dp = opendir(argv[2])) == NULL){
+            printf("can't open %s\n", argv[1]);
+            exit(0);
+        }
+        int count = 0;
+        while ((dirp = readdir(dp)) != NULL)
+            ++count;
+        char *buffer[count];
+        int i = 0;
+        while ((dirp = readdir(dp)) != NULL){
+            buffer[i] = dirp -> d_name;
+            i ++;
+        }
+        int comp(const void *a, const void *b){
+            return (*(int *)a - *(int *)b);
+        }
+        qsort(buffer[count], count, sizeof(long) * 50, comp);
+        i = 0;
+        while (i < count){
+            printf("%s	", buffer[i]);
+            i ++;
+        }
+        }
+
+//ls -a filename
+        case 'a' :{
+        if ((dp = opendir(argv[2])) == NULL){
+            printf("can't open %s\n", argv[1]);
+            exit(0);
+        }
+        while ((dirp = readdir(dp)) != NULL)
+            printf("%s  ", dirp -> d_name);
+        printf("\n");
+        exit(0);
         }
 
 //commend is wrong
